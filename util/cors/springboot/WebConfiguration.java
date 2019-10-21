bf0465efe4cea038c29bcdb39dd45b6bf5ecdf1d
136900923 <136900923@qq.com>
2019-10-21 17:53:46 +0800

    添加cors过滤器文件

---
+++ util/cors/springboot/WebConfiguration.java
@@ -0,0 +1,38 @@
+package com.huyang.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * spring boot指定cors配置文件，当前version为2.1.8
+ */
+@Configuration
+public class WebConfiguration {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        source.registerCorsConfiguration("/api/**", config);
+//        source.registerCorsConfiguration("/sys/**", config);
+//        return new CorsFilter(source);
+//    }
+
+}
